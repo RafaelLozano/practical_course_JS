78a1c210f685ac54f338e4fff9f6b91df267607c
Rafa Lozano <rafael@avena.io>
2021-07-17 23:34:24 -0500

    finish square calc and final styles

--- src/figuras.js
+++ src/figuras.js
@@ -1,10 +1,20 @@
-const calcArea = side => Math.pow(side,2)
+const calcArea = side => Math.pow(side,2);
+const calcPerimeter = side => side * 4;
 const getSquareArea = () => {
     const side = document.getElementById("square side").value
     const result = document.getElementById("result")
     const resultContainer = document.getElementById("result__container")
 
     result.innerHTML = '';//clear existing data
-    result.appendChild(document.createTextNode(calcArea(side)))//
+    result.appendChild(document.createTextNode(`Area: ${calcArea(side)}`))//
+    resultContainer.style.display = "block"//display the result 
+} 
+const getSquarePerimeter = () => {
+    const side = document.getElementById("square side").value
+    const result = document.getElementById("result")
+    const resultContainer = document.getElementById("result__container")
+
+    result.innerHTML = '';//clear existing data
+    result.appendChild(document.createTextNode(`Perimeter: ${calcPerimeter(side)}`))//
     resultContainer.style.display = "block"//display the result 
 } 

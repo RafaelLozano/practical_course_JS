0e57e710cf7a4ae893260259ca6e3485a30aa69d
Rafa Lozano <rafael@avena.io>
2021-07-19 14:53:33 -0500

    support triangle calc area

--- src/figuras.js
+++ src/figuras.js
@@ -1,8 +1,9 @@
 const calcArea = side => Math.pow(side,2);
 const calcPerimeter = side => side * 4;
+const calTringleArea = (base, height) =>  (base*height)/2
+
 const getSquareArea = () => {
     const side = document.getElementById("square side").value
-    const result = document.getElementById("result")
     const resultContainer = document.getElementById("result__container")
 
     resultContainer.innerHTML= `<span class="result__text">Area: ${calcArea(side)} cm<sup>2</sup></span>`
@@ -10,9 +11,16 @@ const getSquareArea = () => {
 } 
 const getSquarePerimeter = () => {
     const side = document.getElementById("square side").value
-    const result = document.getElementById("result")
     const resultContainer = document.getElementById("result__container")
 
     resultContainer.innerHTML= `<span class="result__text">Perimeter: ${calcPerimeter(side)} cm</span>`
+    resultContainer.style.display = "block"//display the result 
+} 
+const getTriangleArea = () => {
+    const base = document.getElementById("triangleBase").value
+    const height = document.getElementById("triangleHeight").value
+    const resultContainer = document.getElementById("result__container--triangle")
+
+    resultContainer.innerHTML= `<span class="result__text">Area: ${calTringleArea(base,height)} cm<sup>2</sup></span>`
     resultContainer.style.display = "block"//display the result 
 } 
